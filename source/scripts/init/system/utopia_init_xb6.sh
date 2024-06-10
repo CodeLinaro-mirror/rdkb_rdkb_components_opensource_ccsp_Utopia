dc563eb15a07cf7a14d940f0c9fe6eba5900ab28
David Fang <david_fang@cable.comcast.com>
2024-06-10 18:03:34 +0000

    BCOMB-2427: Reboot reason is wrong with FirwareDownloadAndFactoryReset
    Reason for change: Added fix to update last reboot reason
    as FirmwareDownloadAndFactoryReset
    Test Procedure: Build and verify
    Test Procedure: None
    Risks: None
    Priority: P1
    
    Change-Id: I731aa2e7d5c18415f0bbba0678bd86342187e4c9
    Signed-off-by: David Fang <david_fang@cable.comcast.com>
    (cherry picked from commit c0b92f6952ef448fff761a93e38ca81576fe520f)
    (cherry picked from commit 228a0d1a3f449549714f5841674a70db209d1145)
    (cherry picked from commit 3f73affa6f245471773c5cc7940bf97bfe087149)

--- source/scripts/init/system/utopia_init_xb6.sh
+++ source/scripts/init/system/utopia_init_xb6.sh
@@ -768,7 +768,15 @@ if [ "$FACTORY_RESET_REASON" = "true" ]; then
        #syscfg set X_RDKCENTRAL-COM_LastRebootReason "WPS-Factory-Reset"
        #syscfg set X_RDKCENTRAL-COM_LastRebootCounter "1"
        rm -f /nvram/WPS_Factory_Reset
-    else
+   elif ([ "${MODEL_NUM}" = "CGM601TCOM" ] || [ "${MODEL_NUM}" = "SG417DBCT" ] || [ "${MODEL_NUM}" = "CVA601ZCOM" ]) && [ -f /nvram/.image_upgrade_and_FR_done ]; then
+       echo "[utopia][init] Detected last reboot reason as FirmwareDownloadAndFactoryReset"
+       if [ -e "/usr/bin/onboarding_log" ]; then
+          /usr/bin/onboarding_log "[utopia][init] Detected last reboot reason as FirmwareDownloadAndFactoryReset"
+       fi
+       syscfg set X_RDKCENTRAL-COM_LastRebootReason "FirmwareDownloadAndFactoryReset"
+       syscfg set X_RDKCENTRAL-COM_LastRebootCounter "1"
+       rm -f /nvram/.image_upgrade_and_FR_done
+   else
        echo "[utopia][init] Detected last reboot reason as factory-reset"
        if [ -e "/usr/bin/onboarding_log" ]; then
           /usr/bin/onboarding_log "[utopia][init] Detected last reboot reason as factory-reset"
