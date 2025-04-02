1f944b5ec27dbc656c8970eae9d5bd87559074e0
nm296 <nareshkumar_m@comcast.com>
2025-04-02 06:37:47 +0000

    RDKB-59336:Enforce SSH connection to lab/test devices
    Reason for change: Add deviceType RFC check before launching dropbear
    Test Procedure: Flash the build , ssh and verify
    Risks: High
    Priority: P1
    
    Signed-off-by: nm296 <nareshkumar_m@comcast.com>
    
    RDKB-59551:Dropbear is not launched with authorized_keys_dev when RFC is Test
    
    Reason for change: check the deviceType RFC and convert it to Upper case
    Test Procedure: Flash the build , ssh and verify
    Risks: High
    Priority: P1
    
    Change-Id: Iaa35e192e899d2792880568c8a43444f8fbebab7
    Signed-off-by: nm296 <nareshkumar_m@comcast.com>

--- source/scripts/init/service.d/service_sshd.sh
+++ source/scripts/init/service.d/service_sshd.sh
@@ -47,6 +47,14 @@ source /etc/waninfo.sh
 WAN_INTERFACE=$(getWanInterfaceName)
 DEFAULT_WAN_INTERFACE="erouter0"
 LANIPV6Support=`sysevent get LANIPv6GUASupport`
+DEVICETYPE=$(dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Identity.DeviceType | grep value | cut -d ":" -f 3 | tr -d ' ' | tr -s ' ' | tr '[:lower:]' '[:upper:]')
+if [ $DEVICETYPE = "TEST" ]; then
+    USE_DEVKEYS="-f authorized_keys_dev"
+    echo_t "[utopia]: dropbear using dev authorization keys"
+else
+    USE_DEVKEYS=""
+    echo_t "[utopia]: dropbear using prod authorization keys"
+fi
 
 if [ "$BOX_TYPE" = "HUB4" ] || [ "$BOX_TYPE" = "SR300" ] || [ "$BOX_TYPE" = "SE501" ] || [ "$BOX_TYPE" = "WNXL11BWL" ] || [ "$BOX_TYPE" = "SR213" ] ||  [ "$BOX_TYPE" == "SCER11BEL" ]; then
    CMINTERFACE=$WAN_INTERFACE
@@ -229,7 +237,7 @@ do_start() {
    getConfigFile $DROPBEAR_PARAMS_2
 
    if ([ "$BOX_TYPE" = "XB6" -a "$MANUFACTURE" = "Arris" ] || [ "$MODEL_NUM" = "INTEL_PUMA" ] || [ "$BOX_TYPE" = "VNTXER5" ] || [ "$BOX_TYPE" = "SCER11BEL" -a "$LANIPV6Support" != "true" ]) ;then
-   	dropbear -E -s -b /etc/sshbanner.txt -a -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 $LISTEN_PARAMS -P $PID_FILE 2>>$CONSOLEFILE
+       dropbear -E -s -b /etc/sshbanner.txt -a -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 $LISTEN_PARAMS -P $PID_FILE $USE_DEVKEYS 2>>$CONSOLEFILE
     if [ -z "$LISTEN_PARAMS" ] ; then
         echo_t "[utopia]: dropbear was not started for erouter0 interface with valid params."
     fi
@@ -248,11 +256,11 @@ do_start() {
    else
        if  ([ "$MANUFACTURE" = "Technicolor" ] || [ "$MODEL_NUM" = "SG417DBCT" ]) ; then
 	  echo dropbear -E -s -K 60 -b /etc/sshbanner.txt ${commandString} -r ${DROPBEAR_PARAMS_1} -r ${DROPBEAR_PARAMS_2} -a -P ${PID_FILE}
-          dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE -K 60 2>>$CONSOLEFILE
+          dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE -K 60 $USE_DEVKEYS 2>>$CONSOLEFILE
        elif ([ "$BOX_TYPE" = "SCER11BEL" -a "$LANIPV6Support" = "true" ]) ; then
-              dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE 2>>$CONSOLEFILE
+              dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE -K 60 $USE_DEVKEYS 2>>$CONSOLEFILE
        else
-   	  dropbear -E -s -b /etc/sshbanner.txt -a -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -p [$CM_IP]:22 -P $PID_FILE 2>>$CONSOLEFILE
+	      dropbear -E -s -b /etc/sshbanner.txt -a -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -p [$CM_IP]:22 -P $PID_FILE $USE_DEVKEYS 2>>$CONSOLEFILE
        fi
    fi
 
